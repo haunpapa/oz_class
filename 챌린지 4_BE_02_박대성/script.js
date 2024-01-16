60abd18a79e89fea83fa2f5ccaf6da3d1773bb58
haunpapa <good78912@gmail.com>
2024-01-16 11:28:12 +0900

    챌린지

---
+++ 챌린지 4_BE_02_박대성/script.js
@@ -0,0 +1,32 @@
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const carousel = document.querySelector('.carousel');
+
+function darkMode(){
+    var body = document.body;
+    body.classList.toggle("dark-Mode");
+    
+    var button = document.getElementById('button');
+    if(button.innerHTML == "Black"){
+        button.innerHTML = "White";
+    } else{
+        button.innerHTML = "Black";
+    }
+};
+
+let index = 0;
+
+prevButton.addEventListener('click', () => {
+    if(index === 0) return ;
+    index -= 1 ;
+
+    carousel.style.transform = `translate3d(-${500 * index}px, 0, 0)`;
+});
+
+nextButton.addEventListener('click',() => {
+    if(index === 2) return;
+    index += 1
+
+    carousel.style.transform = `translate3d(-${500 * index}px, 0, 0)`;
+});
+
